d485f5744a31058c2eca26c9c3f928ca410e6151
Matt Hampel <matth@localdata.com>
2015-11-25 10:54:55 -0500

    Annotate models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id          :integer          not null, primary key
+#  body        :text
+#  question_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#
+
 class Comment < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  slug        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#
+
 class Question < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, :use => :slugged
