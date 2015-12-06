2ee6f405aac641c1b99e99e3c403f45f57a84589
Matt Hampel <matth@localdata.com>
2015-12-06 12:20:38 -0500

    New styling for events pages

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,7 @@ class EventsController < ApplicationController
   layout "events"
 
   def index
-    @events = Event.where('end_date > ?', Time.now)
+    @events = Event.where('end_date >= ?', Time.now)
+    @past_events = Event.where('end_date < ?', Time.now)
   end
 end
