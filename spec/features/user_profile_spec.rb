97390f92af98a613fdff27d442a266afa21f5d72
Matt Hampel <matth@localdata.com>
2016-01-04 15:24:59 -0500

    Add tests for user profile pages

---
+++ spec/features/user_profile_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature 'User profile spec', type: :feature do
+  let!(:user) { FactoryGirl.create :user }
+  let!(:user_two) { FactoryGirl.create :user }
+
+  scenario 'A user creates a profile and visits it' do
+    visit '/users/' + user.id.to_s
+    expect(page).to have_content user.name
+    expect(page).not_to have_content "Edit your account"
+
+    # Log in
+    click_link 'Login'
+    expect(page).to have_content 'Log in'
+    fill_in 'Email', match: :first, with: user.email
+    fill_in 'Password', match: :first, with: user.password
+    click_button 'Log in'
+
+    # The user should have a link to edit their account
+    visit '/users/' + user.id.to_s
+    expect(page).to have_content user.name
+    expect(page).to have_content "Edit your account"
+
+    # the user should not have a linkt o
+    visit '/users/' + user_two.id.to_s
+    expect(page).to have_content user_two.name
+    expect(page).not_to have_content "Edit your account"
+  end
+end
