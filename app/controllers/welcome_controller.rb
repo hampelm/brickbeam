9706681bf32cf48d94dd3ce83ad17b340978e0b4
Matt Hampel <matth@localdata.com>
2016-01-11 13:39:39 -0500

    New homepage design

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @resources = Resource.all
+    @resources = Resource.all.order('updated_at DESC').limit(8)
     @events = Event.where('end_date > ?', Time.now)
+    @questions = Question.where(hidden: [false, nil]).order('created_at DESC').limit(4)
+    @sites = Site.where(hidden: [false, nil]).all
   end
 end
