0eaae3ef0149035e9ebe23148c79da18337d32be
Matt Hampel <matth@localdata.com>
2016-01-18 10:02:56 -0500

    Move rails_admin auth

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,8 +4,8 @@ RailsAdmin.config do |config|
 
   ## == Devise ==
   config.authenticate_with do
-    # redirect_to main_app.root_path unless current_user.try(:is_admin?)
     warden.authenticate! scope: :user
+    redirect_to main_app.root_path unless current_user.try(:is_admin?)
   end
   config.current_user_method(&:current_user)
 
