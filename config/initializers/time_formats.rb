91f9daecda730c54b81195619a1da7607aac6c19
Matt Hampel <matth@localdata.com>
2016-01-18 12:07:06 -0500

    Improve event display

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,3 +1,3 @@
-Time::DATE_FORMATS[:standard] = "%l:%M%P on %b %-m '%y"
+Time::DATE_FORMATS[:standard] = "%l:%M%P on %b %e %Y"
 Time::DATE_FORMATS[:since] = "%B %Y"
 Time::DATE_FORMATS[:date_then_time] = "%B %-m, %Y at %l:%M%P"
