5f5a67d55401a271faea46325c4bd2ebfcc601d8
Matt Hampel <matth@localdata.com>
2016-01-21 16:47:11 -0500

    Clean up downloads and attachment handling

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -15,7 +15,7 @@ RailsAdmin.config do |config|
   ## == PaperTrail ==
   # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
 
-  config.excluded_models = ["Assets", "Attachment", "AttachmentFiles", "Picture", ]
+  # config.excluded_models = ["Assets", "Picture", ]
 
   ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
   config.actions do
