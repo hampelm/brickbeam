338e261c54b4d3ee72192c9f1c14ad7460b1250a
Matt Hampel <matth@localdata.com>
2016-02-15 13:44:28 -0500

    Add tag filter to map

--- app/models/site.rb
+++ app/models/site.rb
@@ -25,6 +25,8 @@
 class Site < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, :use => :slugged
+  serialize :project_tags, JSON
+  # attr_accessible :project_tags
 
   has_many :projects, dependent: :destroy
   accepts_nested_attributes_for :projects
@@ -45,5 +47,14 @@ class Site < ActiveRecord::Base
   def building_size_enum
     ['<1,000', '1,000-1,200', '1,201-1,500', '1,501-2,000', '2,001-3,000', '3,001-5,000', '>5,000']
   end
+
+  # def project_tags
+  #   project_tags = []
+  #   self.projects.each do |project|
+  #     project.tags.each do |tag|
+  #       project_tags.push(tag)
+  #     end
+  #   end
+  # end
 
 end
