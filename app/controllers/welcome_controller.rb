c3496769134c886fe8b856cfa92eb5dd2a7a2210
Matt Hampel <mahatm@gmail.com>
2016-04-26 17:58:11 -0400

    Show closest events first (#195)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     @resources = Resource.all.order('updated_at DESC').limit(8)
-    @events = Event.where('end_date > ?', Time.now)
+    @events = Event.where('end_date > ?', Time.now).order('end_date ASC')
     @questions = Question.where(hidden: [false, nil]).order('created_at DESC').limit(4)
     @sites = Site.where(hidden: [false, nil]).all
 
