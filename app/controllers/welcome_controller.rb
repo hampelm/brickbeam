8518248622ea917516d45c458972b238de0e1a54
Matt Hampel <mahatm@gmail.com>
2019-06-14 21:37:57 -0400

    Switch to future events

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     @resources = Resource.all.order('updated_at ASC').limit(8)
-    @events = Event.where('end_date > ?', Time.now).order(end_date: :asc)
+    @events = Event.future
     @questions = Question.where(hidden: [false, nil]).order('created_at DESC').limit(4)
     @sites = Site.where(hidden: [false, nil]).all
 
